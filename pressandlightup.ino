c93d5efc0118af0a581c267dd47d2bbf4ab08abd
PEN <trannguyenanhoa6.1@gmail.com>
2025-07-11 11:09:08 +0700

    add files

---
+++ pressandlightup.ino
@@ -0,0 +1,56 @@
+int whiteLed = 13;
+int blueLed = 12;
+int redLed = 11;
+int greenLed = 10;
+int yellowLed = 9;
+int buttonPin = 8;
+int buttonState = LOW;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial); 
+  pinMode(buttonPin, INPUT);
+  pinMode(whiteLed, OUTPUT);
+  pinMode(blueLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(yellowLed, OUTPUT);
+}
+
+void loop() {
+  ledOn(whiteLed);
+  buttonWait();
+  ledOff(whiteLed);
+  ledOn(blueLed);
+  buttonWait();
+  ledOff(blueLed);
+  ledOn(redLed);
+  buttonWait();
+  ledOff(redLed);
+  ledOn(greenLed);
+  buttonWait();
+  ledOff(greenLed);
+  ledOn(yellowLed);
+  buttonWait();
+  ledOff(yellowLed);
+}
+
+void ledOn (int led){
+  digitalWrite(led,HIGH);
+}
+
+void ledOff (int led){
+  digitalWrite(led,LOW);
+}
+
+void buttonWait (){
+    while (!buttonState) {
+    buttonState = digitalRead(buttonPin);
+   delay(1);
+  }
+  while (buttonState) {
+    buttonState = digitalRead(buttonPin);
+    delay(1);
+  }
+  buttonState = LOW;
+}
